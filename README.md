0d82c0403b0ec6335ec640af236ef94940075095
Caleb Lawrence <lawrence.calebc@gmail.com>
2025-06-14 10:26:48 -0500

    changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # garland-tx-police-activity
+
+### Running project
+
+```bash
+pipenv run python main.py
+```
+
+### Install a new package
+
+```bash
+pipenv install requests
+```
--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import requests
+from bs4 import BeautifulSoup
 
+base_url = "https://www.garlandtx.gov"
 url = "https://www.garlandtx.gov/396/Crime-Statistics-Maps"
-response = requests.get(url)
+html_response = requests.get(url)
 
-print(response.text)
+soup = BeautifulSoup(html_response.text, "html.parser")
+
+text_to_find = "Previous Week Selected Incident Report (PDF)"
+
+a_tags = soup.find_all(lambda tag: tag.name == "a" and "aria-label" in tag.attrs)
+
+previous_week_link = None
+for a_tag in a_tags:
+    if text_to_find in a_tag.text:
+        previous_week_link = a_tag['href']
+        break
+
+print("Previous Week Selected Incident Report Link:", base_url + previous_week_link)
+
+
+# TODO: download the PDF file
